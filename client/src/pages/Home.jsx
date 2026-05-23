ff4f7df391cbbff85f60cc8a7ed62d2ff11bea38
mazizullah01 <m.azizullah420@gmail.com>
2026-05-23 13:26:05 +0500

    feat(socket): implement join-room event with user mapping and broadcast

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const HomePage = () => {
     return (
         <div className="homepage-container">
-            <div>
+            <div className="input-container">
                 <input type="email" placeholder="Enter your email here" />
                 <input type="text" placeholder="Enter your Room code" />
                 <button>Enter Room</button>
--- server/index.js
+++ server/index.js
@@ -7,7 +7,15 @@ const app = express();
 
 app.use(bodyParser.json());
 
-io.on("connection", socket => {});
+io.on("connection", socket => {
+    socket.on("join-room", (data) => {
+        const { roomId, emailId } = data;
+        console.log("User", emailId, "Joined Room", roomId);
+        emailToSoketMapping.set(emailId, socket.id);
+        socket.join(roomId);
+        socket.broadcast.to(roomId).emit("user-joined", { emailId })
+    });
+});
 
 app.listen(4000,  () => console.log("Http server running at PORT 4000"));
 io.listen(4001);
