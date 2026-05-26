2af30c1650dc94d7336936c93debefb3989f6fc4
mazizullah01 <m.azizullah420@gmail.com>
2026-05-26 13:03:24 +0500

    feat: add SocketProvider with shared Socket.IO connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/Home';
+import { SocketProvider } from "./providers/socket";
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <SocketProvider>
         <Route path="/" element={<HomePage />} />
+        </SocketProvider>
       </Routes>
     </div>
   );
